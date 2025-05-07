2a162fcc9a28006fd4906aaebee4b6533c14afd3
antoniofelices <antonio@studiomoare.com>
2025-05-07 16:25:27 +0200

    add function hoursToMinutes

--- src/films.js
+++ src/films.js
@@ -71,7 +71,35 @@ function moviesAverageByCategory(movies, genre) {
 }
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {}
+function hoursToMinutes(movies) {
+    let moviesDurationByMinuts = []
+
+    if (!movies) return moviesDurationByMinuts
+
+    moviesDurationByMinuts = movies.map((movie) => {
+        const minutesToHour = 60
+        let duration = movie.duration
+        let durationArray = []
+        let hours = ''
+        let minutes = 0
+        let totalMinutes = 0
+
+        durationArray = duration.split(' ')
+        hours = Number(durationArray[0].replace('h', ''))
+        minutes = durationArray[1]
+            ? Number(durationArray[1].replace('min', ''))
+            : 0
+
+        totalMinutes = hours * minutesToHour + minutes
+
+        return {
+            ...movie,
+            duration: totalMinutes,
+        }
+    })
+
+    return moviesDurationByMinuts
+}
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {}
